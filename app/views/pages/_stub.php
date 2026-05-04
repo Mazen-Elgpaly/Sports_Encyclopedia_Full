68915f862ae7328a8074466355c67538e21af44a
Mazen <mazenelgpaly180@gmail.com>
2026-05-04 11:27:47 -0700

    commit

--- app/views/pages/_stub.php
+++
@@ -1,5 +0,0 @@
-<?php
-// This file is a router for simple static pages.
-// Each page is output based on which view is being rendered.
-// The actual content is in the individual page files below.
-?>
